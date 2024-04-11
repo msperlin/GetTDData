d2883106f2f191c396ddb642a324553914b21da4
msperlin <marceloperlin@gmail.com>
2024-04-11 14:21:51 -0300

    added cli message in td_get_current()

--- R/td_get_current.R
+++ R/td_get_current.R
@@ -1,6 +1,6 @@
 #' Returns current TD prices
 #'
-#' Fetches current prices of TD assets from website's json api
+#' Fetches current prices of TD assets from website's json api at <https://www.tesourodireto.com.br/titulos/precos-e-taxas.htm>
 #'
 #' @return a dataframe with prices
 #' @export
@@ -8,6 +8,9 @@
 #' @examples
 #' td_get_current()
 td_get_current <- function() {
+
+  cli::cli_alert_info("fetching current TD prices")
+
   url <- "https://www.tesourodireto.com.br/json/br/com/b3/tesourodireto/service/api/treasurybondsinfo.json"
   l_out <- jsonlite::fromJSON(url)
 
@@ -20,6 +23,8 @@ td_get_current <- function() {
                   annual_ret = anulRedRate/100) |>
     dplyr::select(name, maturity, min_qtd, min_value,
                   price, annual_ret)
+
+  cli::cli_alert_success("got dataframe with {nrow(df_current)} rows and {ncol(df_current)} columns")
 
   return(df_current)
 }
